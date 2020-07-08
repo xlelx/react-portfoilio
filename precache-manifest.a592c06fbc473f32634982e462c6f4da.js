95692351d9f92744f2663e2fbf5ce188b4458eea
Khant Thurein Han <khantthurein@student.unimelb.edu.au>
2020-07-08 23:49:40 +1000

    Updates

---
+++ precache-manifest.a592c06fbc473f32634982e462c6f4da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a992dcb092ba42c6b4c20b18402e53e1",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "9ca7c5ba3ed5bffe180d",
+    "url": "/react-portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "82ae99bbb3e0e22547f4",
+    "url": "/react-portfolio/static/js/2.11283df0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.11283df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ca7c5ba3ed5bffe180d",
+    "url": "/react-portfolio/static/js/main.e4206f19.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
