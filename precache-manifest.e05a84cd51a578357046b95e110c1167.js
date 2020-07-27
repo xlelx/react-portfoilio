fa8924f8e05d51abbd92be9979ba593dd6c64521
Khant Thurein Han <khantthurein@student.unimelb.edu.au>
2020-07-28 04:08:53 +1000

    Updates

--- precache-manifest.e05a84cd51a578357046b95e110c1167.js
+++ precache-manifest.e05a84cd51a578357046b95e110c1167.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f27fd386af91dcf7c162bb468bffb458",
+    "revision": "e39c1adc19d852cee849de5247b9798f",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "b4c9bea0b0db92ba2f2b",
+    "revision": "745c64bdb0806e302ab4",
     "url": "/react-portfolio/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/js/2.11283df0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b4c9bea0b0db92ba2f2b",
-    "url": "/react-portfolio/static/js/main.7efc395b.chunk.js"
+    "revision": "745c64bdb0806e302ab4",
+    "url": "/react-portfolio/static/js/main.9c0b0b09.chunk.js"
   },
   {
     "revision": "aff33cef9dbb8787d6b3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.e277efbd1b10b524efb0b39b86c93b4c.js"
+  "/react-portfolio/precache-manifest.e05a84cd51a578357046b95e110c1167.js"
 );
 
 self.addEventListener('message', (event) => {
